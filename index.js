7b8d18d0d3140e32edb707fb848dc0c5914c0763
NisalDilshanGinige <166273277+NisalDilshanGinige@users.noreply.github.com>
2026-05-24 18:12:37 +0530

    index.js

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-const express = require('express');
-const app = express();
-app.use(express.json());
-
-const VERIFY_TOKEN = 'lankabusfindertoken';
-
-app.get('/webhook', (req, res) => {
-  const mode = req.query['hub.mode'];
-  const token = req.query['hub.verify_token'];
-  const challenge = req.query['hub.challenge'];
-  if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-    res.status(200).send(challenge);
-  } else {
-    res.sendStatus(403);
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+    
+    if (mode === 'subscribe' && token === 'lankabusfindertoken') {
+      res.status(200).send(challenge);
+    } else {
+      res.status(403).send('Forbidden');
+    }
+  } else if (req.method === 'POST') {
+    res.status(200).send('OK');
   }
-});
-
-app.post('/webhook', (req, res) => {
-  res.sendStatus(200);
-});
-
-app.listen(3000, () => console.log('Webhook running!'));
+}
