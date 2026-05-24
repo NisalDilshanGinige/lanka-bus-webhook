f814b54a1618358f76010af114dc1174ff749028
NisalDilshanGinige <166273277+NisalDilshanGinige@users.noreply.github.com>
2026-05-24 18:21:17 +0530

    index.js

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
 export default function handler(req, res) {
-  if (req.method === 'GET') {
-    const mode = req.query['hub.mode'];
-    const token = req.query['hub.verify_token'];
-    const challenge = req.query['hub.challenge'];
-    
-    if (mode === 'subscribe' && token === 'lankabusfindertoken') {
-      res.status(200).send(challenge);
-    } else {
-      res.status(403).send('Forbidden');
-    }
-  } else if (req.method === 'POST') {
-    res.status(200).send('OK');
-  }
+  res.status(200).send('Lanka Bus Webhook Running!');
 }
