0914de00e9ad551360bdf7f0eb827d914287d7b2
NisalDilshanGinige <166273277+NisalDilshanGinige@users.noreply.github.com>
2026-05-24 19:30:35 +0530

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,4 +1,8 @@
-module.exports = (req, res) => {
+const fetch = require('node-fetch');
+
+const N8N_URL = 'https://nisaldilshan.app.n8n.cloud/webhook/9ae649e4-751a-4e8b-8f10-30dca987ed24';
+
+module.exports = async (req, res) => {
   if (req.method === 'GET') {
     const mode = req.query['hub.mode'];
     const token = req.query['hub.verify_token'];
@@ -8,7 +12,12 @@ module.exports = (req, res) => {
     } else {
       res.status(403).send('Forbidden');
     }
-  } else {
+  } else if (req.method === 'POST') {
+    await fetch(N8N_URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(req.body)
+    });
     res.status(200).send('OK');
   }
 };
